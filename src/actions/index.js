c49463db328271b43bfeae45759516e1c4ee560a
Edan Chetrit <edan.chetrit@gmail.com>
2017-04-15 19:56:35 +0300

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'cdd60fd50152e880bb840595c8eadec0';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeahter(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,15 @@
 import { combineReducers } from 'redux';
+import WeatherReducer from './reducer_weather';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: WeatherReducer
 });
 
 export default rootReducer;
+
+
+
+// const rootReducer = combineReducers({
+//   books: BooksReducer,
+//   activeBook: ActiveBook
+// });
